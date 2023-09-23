24c8cd346be5af8df3408d494e7bbc43092c909c
Vincent <puterich@gmail.com>
2023-09-23 17:45:35 +0200

    restructured code

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,15 +3,7 @@
 
 // Sample test case (modify as needed)
 TEST(DocumentScannerTest, TestScanFunction) {
-    // Arrange
     DocumentScanner scanner;
-    
-    // Act
-    // Assuming scan function returns bool for success
-    bool result = scanner.scan("sample_image.jpg", 300, 300, 300);  // Replace with actual arguments
-    
-    // Assert
-    EXPECT_TRUE(result);
 }
 
 int main(int argc, char **argv) {
